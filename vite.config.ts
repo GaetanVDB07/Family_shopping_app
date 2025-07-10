c1bc185bae4aa118251159a575c880ca9ad8d963
GaetanVDB07 <gaetan.vandenberghe@hotmail.be>
2025-07-10 15:31:26 +0200

    trying to solve issues

--- vite.config.ts
+++ vite.config.ts
@@ -16,9 +16,8 @@ export default defineConfig({
       "@assets": path.resolve(__dirname, "attached_assets"),
     },
   },
-  root: path.resolve(__dirname, "client"),
   build: {
-    outDir: path.resolve(__dirname, "dist/public"),
+    outDir: path.resolve(__dirname, "dist"),
     emptyOutDir: true,
   },
   server: {
