ae41c5fbeeb23506e9d82f9f97db2a90af6198dc
GaetanVDB07 <gaetan.vandenberghe@hotmail.be>
2025-07-18 19:56:36 +0200

    changed vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,6 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
 import path from "path";
 import { fileURLToPath } from "url";
 
@@ -8,6 +9,7 @@ const __dirname = path.dirname(fileURLToPath(import.meta.url));
 export default defineConfig({
   plugins: [
     react(),
+    tailwindcss(),
   ],
   resolve: {
     alias: {
